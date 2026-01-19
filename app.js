3670c2a0e8716b3d87c25b76a1361bb838a3a21b
sakshimirajkar <mirajkarsakshi421@gmail.com>
2026-01-19 15:33:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+// ---------- FLAG FUNCTION ----------
+function updateFlag(element) {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+}
+
+// ---------- DROPDOWNS ----------
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = true;
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = true;
+    }
+
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+// ---------- EXCHANGE RATE ----------
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    const rate = data.rates[toCurr.value];
+    const finalAmount = (amtVal * rate).toFixed(2);
+
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Network error";
+    console.error(error);
+  }
+};
+
+// ---------- BUTTON ----------
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+// ---------- AUTO LOAD ----------
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
